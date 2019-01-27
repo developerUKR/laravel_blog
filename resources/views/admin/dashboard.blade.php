bc47a4136eeaa7e44cef049b33b055499940d9aa
Vadym Fomin <developer2@ukr.net>
2019-01-27 14:39:27 +0300

    Create admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,49 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Категорий 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Новостей 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Посетителей 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Сегодня 0</span></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Создать категорию</a>
+                    <a class="list-group-item" href="#">
+                        <h4 class="list-group-item-heading">Категория первая</h4>
+                        <p class="list-group-item-text">
+                          Каличество новостей
+                        </p>
+                    </a>
+            </div>
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Создать новость</a>
+                    <a class="list-group-item" href="№">
+                        <h4 class="list-group-item-heading">Новость первая</h4>
+                        <p class="list-group-item-text">
+                           Категория
+                        </p>
+                    </a>
+            </div>
+        </div>
+    </div>
+@endsection
